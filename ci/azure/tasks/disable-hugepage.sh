765a6ab96b2444a81f168ed2ff7c0b86fc890f6d
Sani Chabi Yo <schabiyo@schabiyo.local>
2017-12-13 15:06:22 -0500

    typo

--- ci/azure/tasks/disable-hugepage.sh
+++ ci/azure/tasks/disable-hugepage.sh
@@ -5,22 +5,14 @@ touch opsmgr-pipeline/ci/azure/tasks/ansible/opsmgr-host
 printf "%s\n" "[OpsManager]" >> opsmgr-pipeline/ci/azure/tasks/ansible/opsmgr-host
 printf "%s\n" "${OPSMGR_SERVER_HOSTNAME}.${AZURE_RESOURCE_LOCATION}.cloudapp.azure.com" >> opsmgr-pipeline/ci/azure/tasks/ansible/opsgmr-host
 
-sed -i -e "s@AZURE_SERVER_ADMIN@${AZURE_SERVER_ADMIN}@g" opsmgr-pipeline/ci/azure/tasks/ansible/playbook-create-opsmgr-vm.yml
+sed -i -e "s@AZURE_SERVER_ADMIN@${AZURE_SERVER_ADMIN}@g" opsmgr-pipeline/ci/azure/tasks/ansible/playbook-disable-hugepage.yml
 
 
-# Init ssh folder and Copy ssh key file
 #Get the SSH key from the configs adn add it to the ssh folder
 mkdir ~/.ssh
-
-
-# Add this to the config file
-echo -e "Host=${OPSMGR_SERVER_HOSTNAME}.${AZURE_RESOURCE_LOCATION}.cloudapp.azure.com\nIdentityFile=~/.ssh/id_rsa\nUser=${AZURE_SERVER_ADMIN}" >> ~/.ssh/config
-chmod 600 ~/.ssh/config
-chmod 600 ~/.ssh/id_rsa*
-
-
-
+#Get the keys generate by previous task instead of regenerating them
+cp keys-out/* ~/.ssh/
 
 cd opsmgr-pipeline/ci/azure/tasks/ansible/
- ansible-playbook -i opsmgr-host playbook-create-opsmgr-vm.yml --private-key ~/.ssh/id_rsa
+ ansible-playbook -i opsmgr-host playbook-disable-hugepage.yml --private-key ~/.ssh/id_rsa
 cd ..
