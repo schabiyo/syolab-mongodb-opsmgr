0cee298a77b9da84e2b3bbcca80cec2686e841aa
Sani Chabi Yo <schabiyo@schabiyo.local>
2017-12-13 15:19:14 -0500

    typo in path

--- ci/azure/tasks/disable-hugepage.sh
+++ ci/azure/tasks/disable-hugepage.sh
@@ -5,7 +5,7 @@ touch opsmgr-pipeline/ci/azure/tasks/ansible/opsmgr-host
 printf "%s\n" "[OpsManager]" >> opsmgr-pipeline/ci/azure/tasks/ansible/opsmgr-host
 printf "%s\n" "${OPSMGR_SERVER_HOSTNAME}.${AZURE_RESOURCE_LOCATION}.cloudapp.azure.com" >> opsmgr-pipeline/ci/azure/tasks/ansible/opsgmr-host
 
-sed -i -e "s@AZURE_SERVER_ADMIN@${AZURE_SERVER_ADMIN}@g" opsmgr-pipeline/ci/azure/tasks/ansible/playbook-disable-hugepage.yml
+sed -i -e "s@AZURE_SERVER_ADMIN@${AZURE_SERVER_ADMIN}@g" opsmgr-pipeline/ci/azure/tasks/ansible/playbook-hugepage.yml
 
 
 #Get the SSH key from the configs adn add it to the ssh folder
