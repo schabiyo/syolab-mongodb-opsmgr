51372accffba9fb1f196bc99c375f15312130b25
Sani Chabi Yo <schabiyo@schabiyo.local>
2017-12-13 16:24:11 -0500

    disable numa

--- ci/azure/tasks/disable-numa.sh
+++ ci/azure/tasks/disable-numa.sh
@@ -3,14 +3,17 @@ set -e
 
 sed -i -e "s@OPSMGR_SERVER_HOSTNAME@${OPSMGR_SERVER_HOSTNAME}@g" opsmgr-pipeline/ci/azure/tasks/ansible/opsmgr-host
 sed -i -e "s@AZURE_RESOURCE_LOCATION@${AZURE_RESOURCE_LOCATION}@g" opsmgr-pipeline/ci/azure/tasks/ansible/opsmgr-host
-sed -i -e "s@AZURE_SERVER_ADMIN@${AZURE_SERVER_ADMIN}@g" opsmgr-pipeline/ci/azure/tasks/ansible/playbook-hugepage.yml
+sed -i -e "s@AZURE_SERVER_ADMIN@${AZURE_SERVER_ADMIN}@g" opsmgr-pipeline/ci/azure/tasks/ansible/playbook-numa.yml
 
 
+# Init ssh folder and Copy ssh key file
 #Get the SSH key from the configs adn add it to the ssh folder
+# Add this to the config file
 mkdir ~/.ssh
 #Get the keys generate by previous task instead of regenerating them
+
 cp keys-out/* ~/.ssh/
 
 cd opsmgr-pipeline/ci/azure/tasks/ansible/
- ansible-playbook -i opsmgr-host playbook-hugepage.yml --private-key ~/.ssh/id_rsa
+ ansible-playbook -i opsmgr-host playbook-numa.yml --private-key ~/.ssh/id_rsa
 cd ..
