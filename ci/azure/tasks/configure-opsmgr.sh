0c0fd20db0c0b919a0e93606113ec1e0fdcc08fa
Sani Chabi Yo <chabiyosani@gmail.com>
2017-12-26 13:34:31 -0500

    ssl configs

--- ci/azure/tasks/configure-opsmgr.sh
+++ ci/azure/tasks/configure-opsmgr.sh
@@ -3,8 +3,48 @@ set -e
 
 ## Use sed to replace the parameters in configureOpsMrg.jsvar
 echo "Configuring Ops Manager"
+## This depends if SSL is supposed to be configured or not
+## Check if a key pem has been specified
 
-opsUrl="http://${OPSMGR_SERVER_HOSTNAME}.${AZURE_RESOURCE_LOCATION}.cloudapp.azure.com:8080";
+opsDomain="${OPSMGR_SERVER_HOSTNAME}.${AZURE_RESOURCE_LOCATION}.cloudapp.azure.com";
+
+if [ ${#OPSMGR_CENTRAL_URL} ge 4];then
+    opsDomain = ${OPSMGR_CENTRAL_URL}
+fi
+
+
+if [ ${#OPSMGR_PEM} ge 50 ]; then
+
+  #Get the SSH key from the configs adn add it to the ssh folder
+  mkdir ~/.ssh
+  #Had to do this as the key is being read in one single line
+  printf "%s\n" "-----BEGIN RSA PRIVATE KEY-----" >> ~/.ssh/id_rsa
+  printf "%s\n" $OPSMGR_SSHKEY_PRIVATE | tail -n +5 | head -n -4 >>  ~/.ssh/id_rsa
+  printf "%s" "-----END RSA PRIVATE KEY-----" >> ~/.ssh/id_rsa
+
+  echo $OPSMGR_SSHKEY_PUBLIC >> ~/.ssh/id_rsa.pub
+
+  # Create the PEM file
+  printf "%s\n" "-----BEGIN RSA PRIVATE KEY-----" >> ~/opsmanager.pem
+  printf "%s\n" $OPSMGR_PEM | tail -n +5 | head -n -4 >>  ~/opsmanager.pem
+  printf "%s" "-----END RSA PRIVATE KEY-----" >> ~/opsmanager.pem
+
+
+  # Add this to the config file
+  echo -e "Host=${OPSMGR_SERVER_HOSTNAME}.${AZURE_RESOURCE_LOCATION}.cloudapp.azure.com\nIdentityFile=~/.ssh/id_rsa\nUser=${AZURE_SERVER_ADMIN}" >> ~/.ssh/config
+  chmod 600 ~/.ssh/config
+  chmod 600 ~/.ssh/id_rsa*
+  chmod 600 ~/opsmanager.pem
+
+  cd opsmgr-pipeline/ci/azure/tasks/ansible/
+   ansible-playbook -i opsmgr-host playbook-upload-pem.yml --private-key ~/.ssh/id_rsa
+  cd ..
+
+  opsUrl="https://${opsDomain}:8443";
+else
+  opsUrl="http://${opsDomain}:8080";
+fi
+
 
 sed -i -e "s~OPSMGR_URL~${opsUrl}~g" opsmgr-pipeline/ci/js/config.json
 sed -i -e "s~OPSMGR_REGISTRATION_USERNAME~${OPSMGR_REGISTRATION_USERNAME}~g" opsmgr-pipeline/ci/js/config.json
@@ -18,6 +58,9 @@ sed -i -e "s~OPSMGR_CONFIG_EMAIL_ADMIN~${OPSMGR_CONFIG_EMAIL_ADMIN}~g" opsmgr-pi
 sed -i -e "s@OPSMGR_CONFIG_EMAIL_TRANSPORT@${OPSMGR_CONFIG_EMAIL_TRANSPORT}@g" opsmgr-pipeline/ci/js/config.json
 sed -i -e "s@OPSMGR_CONFIG_EMAIL_HOSTNAME@${OPSMGR_CONFIG_EMAIL_HOSTNAME}@g" opsmgr-pipeline/ci/js/config.json
 sed -i -e "s@OPSMGR_CONFIG_EMAIL_PORT@${OPSMGR_CONFIG_EMAIL_PORT}@g" opsmgr-pipeline/ci/js/config.json
+
+
+## Run the playbook that will upload the pem file to the host
 
 ## run node
 
