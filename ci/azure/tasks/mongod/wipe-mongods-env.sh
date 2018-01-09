b4f745ad2c4fbc7c8a1fbb8cd46003affd4179a0
Sani Chabi Yo <chabiyosani@gmail.com>
2018-01-09 09:58:11 -0500

    wipe mongods

---
+++ ci/azure/tasks/mongod/wipe-mongods-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+sed -i -e "s@AZURE_RESOURCE_GROUP@${AZURE_RESOURCE_GROUP}@g" opsmgr-pipeline/ci/azure/tasks/mongod/ansible/playbook-wipe-mongods-env.yml
+
+
+cd opsmgr-pipeline/ci/azure/tasks/mongods/ansible/
+ ansible-playbook  playbook-wipe-mongods-env.yml
+cd ..
