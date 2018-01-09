722818adc5288c48d3d4c61449f21983569e967d
Sani Chabi Yo <chabiyosani@gmail.com>
2018-01-09 13:55:37 -0500

    typo

--- ci/azure/tasks/mongod/wipe-mongods-env.sh
+++ ci/azure/tasks/mongod/wipe-mongods-env.sh
@@ -4,6 +4,6 @@ set -e
 sed -i -e "s@AZURE_RESOURCE_GROUP@${AZURE_RESOURCE_GROUP}@g" opsmgr-pipeline/ci/azure/tasks/mongod/ansible/playbook-wipe-mongods-env.yml
 
 
-cd opsmgr-pipeline/ci/azure/tasks/mongods/ansible/
+cd opsmgr-pipeline/ci/azure/tasks/mongod/ansible/
  ansible-playbook  playbook-wipe-mongods-env.yml
 cd ..
