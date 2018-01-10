96d887db65706fcd2a486d04215b4844655e8ea3
Sani Chabi Yo <chabiyosani@gmail.com>
2018-01-10 03:44:06 -0500

    update pipeline

--- ci/azure/deploy-pipeline.sh
+++ ci/azure/deploy-pipeline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PIPELINE_NAME=mongodb-opsmgr
-ALIAS=syolab
+PIPELINE_NAME=mongodb-mms
+ALIAS=ci.syolab
 CREDENTIALS="credentials.yml"
 
-echo y | ./../../local_fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c pipeline.yml -l "${CREDENTIALS}"
-./../../local_fly -t "${ALIAS}" expose-pipeline  -p  "${PIPELINE_NAME}"
+echo y | fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c pipeline.yml -l "${CREDENTIALS}"
+fly -t "${ALIAS}" expose-pipeline  -p  "${PIPELINE_NAME}"
